e6f8258e6253bca61466e4b85ea79bd41aaf7d1a
arianacarnielli <ariana.carnielli@gmail.com>
2020-12-13 18:37:25 +0100

    commentaires ajoutés

--- grille.py
+++ grille.py
@@ -2,7 +2,6 @@
 """
 Created on Mon Dec  7 21:01:59 2020
 
-@author: arian
 """
 import random
 import time
@@ -13,12 +12,13 @@ from collections import defaultdict
 
 class Grille():
     """
-    Représente la grille. 
-    La grille est représentée par un array numpy 2D d’entiers, chaque entier 
-    représentant une couleur. 
-    Initialise la grille des couleurs de façon aléatoire et contient une 
-    méthode pour initialiser des poids de façon aussi aléatoire dans une 
-    deuxième grille auxiliaire.
+    Représente la grille.
+    La grille est représentée par 2 arrays numpy 2D d’entiers, une où chaque 
+    entier représente une couleur et une ou chaque entier représente un poids 
+    entre 1 et 9.
+    Initialise la grille des couleurs et des poids de façon aléatoire en 
+    utilisant la liste de probabilités proba_coul pour la grille de couleurs et
+    la liste de probabilités proba_nb pour les poids. 
     
     Parameters
     ----------
@@ -27,19 +27,18 @@ class Grille():
     nb_col : int
         Nombre de colonnes de la grille.
     tab_cost : list(int)
-        Liste des coûts de chaque couleur. Sa taille doit être égale au 
-        paramètre couleurs. Le défaut est [1, 2, 3, 4].
+        Liste des coûts de chaque couleur. Le défaut est [1, 2, 3, 4].
     p : float
-        Probabilité d’atteindre la case cible. Le défault vaut 1.
+        Probabilité d’atteindre la case cible. Le défaut vaut 1.
     proba_coul : list(float)
         Liste des probabilités pour l'occurrence de chaque couleur 
-        (taille égale à la quantité de couleurs) si None, probabilité uniforme.
+        (taille égale à la taille de tab_cost) si None, probabilité uniforme.
         Le défaut est None.
     proba_mur : float
         Probabilité d'occurrence des murs. Le défaut vaut 0.
     proba_nb : list(float)
-            Liste des probabilités pour l'occurrence de chaque chiffre entre 1 
-            et 9. Si None, probabilité uniforme. Le défault est None.
+    Liste des probabilités pour l'occurrence de chaque chiffre entre 1 
+    et 9. Si None, probabilité uniforme. Le défaut est None.
         
     Attributes
     ----------
@@ -58,7 +57,7 @@ class Grille():
     
     def __init__(self, nb_lig, nb_col, tab_cost = [1, 2, 3, 4], p = 1, proba_coul = None, proba_mur = 0, proba_nb = None):
         couleurs = len(tab_cost)
-        #On confirme que les tailles sont bonnes
+        # On confirme que les tailles des tableaux sont bonnes
         assert (proba_coul is None) or (len(proba_coul) == couleurs), "Tableau des probabilités des couleurs avec la mauvaise taille"   
         
         #On calcule les probabilités des couleurs et murs (les probas des couleurs sont conditionnelles au début)
@@ -66,13 +65,13 @@ class Grille():
         proba_tab[0] = proba_mur
         proba_tab[1:] = proba_coul or (np.ones(couleurs) / couleurs)
         proba_tab[1:] *= (1 - proba_mur)  
-        #Création de la grille
+        # Création de la grille de couleurs
         self.tab = np.random.choice(couleurs + 1, (nb_lig, nb_col), p = proba_tab) - 1
+        # Création de la grille de poids
+        self.chiffre = np.random.choice(9, self.tab.shape, p = proba_nb) + 1
         self.tab_cost = tab_cost
         self.p = p
-        self.chiffre = np.random.choice(9, self.tab.shape, p = proba_nb) + 1
-        
-
+      
     def proba_trans(self, i, j, action):
         """
         Calcule les cases atteignables a partir d’une case (i, j) quelconque en
@@ -88,13 +87,13 @@ class Grille():
             L’action à réaliser. L’encodage est 0-haut, 1-droite, 2-bas,
             3-gauche.
             
-        Attributes
-        ----------
+        Returns
+        -------
         cases_p : defaultdict((int, int), float)
             Dictionnaire indexé par les cases atteignables et contenant les 
             probabilités d’atteindre chaque case.
         """
-        
+        # On utilise un defaultdict pour avoir une valeur par défaut
         cases_p = defaultdict(lambda: 0)
         # On teste si la case cible est atteignable et après on calcule leur 
         # voisinage dans le cas positif. Dans le cas negatif, on reste sur la
@@ -133,6 +132,26 @@ class Grille():
         return cases_p
     
     def proba_trans_arr(self, i, j):
+        """
+        Calcule un dictionnaire avec les probabilités T((i’, j’), a, (i, j)), 
+        utilisé dans les calculs des PL. 
+        Les clés sont des triplets (i’, j’, a) des cases qui peuvent atteindre 
+        une case (i, j) en faisant l’action a et les valeurs sont les 
+        probabilités d'atteindre la case (i, j). 
+        
+        Parameters
+        ----------
+        i : int
+            Indice de ligne de la case.
+        j : int
+            Indice de colonne de la case.
+        
+        Returns
+        -------
+        cases_p : defaultdict((int, int), float)
+            Dictionnaire indexé par les cases et actions et contenant les 
+            probabilités d’atteindre la case (i, j).
+        """
         arr = {}
         if self.case_possible(i - 1, j - 1):
             if self.case_possible(i - 1, j):
@@ -197,9 +216,44 @@ class Grille():
         return arr
         
     def case_possible(self, i, j):
+        """
+        Retourne True si la case (i, j) est atteignable, False sinon.
+        
+        Parameters
+        ----------
+        i : int
+            Indice de ligne de la case.
+        j : int
+            Indice de colonne de la case.
+        
+        Returns
+        -------
+        boolean 
+            True si case atteignable, False sinon.
+        """
         return i >= 0 and j >= 0 and i < self.tab.shape[0] and j < self.tab.shape[1] and self.tab[i, j] >= 0
       
     def case_cout(self, i, j, mode):
+        """
+        Retourne le coût de la case (i, j) d’après le mode de calcul. 
+        Deux modes: ‘couleur’ et ‘somme_chiffre’.
+        Mode ‘couleur’ retourne le coût basé sur le tableau tab_coul.
+        Mode ‘somme_chiffre’ retourne le coût basé sur le tableau chiffre.
+
+        Parameters
+        ----------
+        i : int
+            Indice de ligne de la case.
+        j : int
+            Indice de colonne de la case.
+        mode : String
+            Le mode de calcul du coût. 'couleur' ou 'somme_chiffre'.
+
+        Returns
+        -------
+        int :
+            Coût de la case d'après le mode choisi.
+        """
         if mode == "couleur":
             return self.tab_cost[self.tab[i, j]]
         elif mode == "somme_chiffre":
@@ -250,22 +304,22 @@ class Visualisation():
         
     def view(self, case_px = 40, mode = "couleur", strategy = None):
         """
-        Créé une visualisation de la grille dans une fenêtre à l’aide du module
-        Tkinter. 
+        Permet la visualisation de la grille dans une fenêtre à l’aide du 
+        module Tkinter. 
 
         Parameters
         ----------
         case_px : int
             La taille en pixels d’une case de la grille. Le défaut est 40.
         mode : string
-            DESCRIPTION. The default is "couleur".
-        strategy : TYPE, optional
-            DESCRIPTION. The default is None.
-
-        Returns
-        -------
-        None.
-
+            Le mode de visualisation. Peut être 'couleur' ou 'chiffre'. 
+            The default is 'couleur'. 
+            Le mode 'couleur' correspond à la grille présenté dans la partie 2
+            et 3. Le mode 'chiffre' correspond à la grille présenté dans la 
+            partie 4.
+        strategy : numpy.ndarray
+            Tableau représentant une stratégie. Peut être 2D (si stratégie 
+            pure) ou 3D (si stratégie mixte). The default is None.
         """
         self.case_px = case_px
         self.strategy = strategy
@@ -292,19 +346,19 @@ class Visualisation():
         self._y0 = 20
      
         # On dessine la grille
-        self.dessin_grid(largeur, hauteur)
+        self._dessin_grid(largeur, hauteur)
         
         # Si on a une stratégie passé en argument et si c'est une stratégie déterministe alors on dessine des flèches indiquant où aller
         if strategy is not None and strategy.ndim == 2:
             # Code ASCII des flèches
             self._direct = ["\u2191", "\u2192", "\u2193", "\u2190"]
-            self.dessin_fleche() 
+            self._dessin_fleche() 
         
         # Correspond à la grille présenté dans la partie 2 et 3 du projet
         if mode == "couleur":
             # Text où on montre la valeur totale des coûts pour la grille de type "chiffre"
             self._totalcosts = None 
-            self.dessin_couleur()
+            self._dessin_couleur()
             self._costs.append(0)
             wg = tk.Label(window, text = self._costs[0], fg = "#5E5E64", font = "Verdana " + str(int(-0.5 * self.case_px)) + " bold")
             wg.pack(side = tk.LEFT, padx = 5, pady = 5)
@@ -313,6 +367,7 @@ class Visualisation():
         # Correspond à la grille présenté dans la partie 4 du projet     
         elif mode == "chiffre":
             self.dessin_chiffre()
+            # On a un coût séparé par couleur
             for i in range(len(self.tab_coul)):
                 self._costs.append(0)
                 wg = tk.Label(window, text = self._costs[i], fg = self.tab_coul[i], font = "Verdana " + str(int(-0.5 * self.case_px)) + " bold")
@@ -360,9 +415,9 @@ class Visualisation():
         # Traduction des touches vers les chiffres
         dict_dir = {"Up": 0, "Right": 1, "Down": 2, "Left": 3}
         touche = event.keysym
-        # Si une des flèches, on la passe à la fonction move directement
+        # Si une des flèches, on la passe à la fonction _move directement
         if touche in dict_dir:
-            self.move(dict_dir[touche])
+            self._move(dict_dir[touche])
         # Sinon et si on a une stratégie, on tire une flèche soit de façon déterministe, soit d'après la loi de probabilité de la case courante 
         elif touche == "space" and self.strategy is not None:
             j = round((self._x0 - 20) / self.case_px)
@@ -370,9 +425,12 @@ class Visualisation():
             strat = self.strategy[i,j]
             if self.strategy.ndim != 2:
                 strat = np.random.choice(4, p = strat)  
-            self.move(strat)
+            self._move(strat)
         
-    def move(self, action):
+    def _move(self, action):
+        """
+        Bouge le robot et la flèche en dessus si elle existe. 
+        """
         j = round((self._x0 - 20) / self.case_px)
         i = round((self._y0 - 20) / self.case_px)
     
@@ -390,6 +448,7 @@ class Visualisation():
             self._canevas.itemconfig(self.fleche_pion, text = self._direct[self.strategy[place]])
             self._canevas.coords(self.fleche_pion, self._x0 + self.case_px // 2, self._y0 + self.case_px // 2) 
         
+        # Si on a _totalcosts, alors on est dans le 2ème type de grille avec des poids séparés par couleur
         if self._totalcosts is not None:
             indice = self.grille.tab[i, j]
             self._costs[indice] += self.grille.case_cout(i, j, "somme_chiffre")
@@ -400,7 +459,11 @@ class Visualisation():
             self._costs[indice] += self.grille.case_cout(i, j, "couleur")
             self._costs_labels[indice].config(text = str(self._costs[indice]))
  
-    def dessin_fleche(self):
+    def _dessin_fleche(self):
+        """
+        Dessine les flèches indiquant la meilleure choix si on a une stratégie 
+        pure. On dessine aussi une flèche en dessus du pion.
+        """
         for i in range(self.grille.tab.shape[0]):
             for j in range(self.grille.tab.shape[1]):
                 x0 = 20 + self.case_px * j 
@@ -413,7 +476,10 @@ class Visualisation():
 
         self.fleche_pion = self._canevas.create_text(20 + self.case_px // 2, 20 + self.case_px // 2, anchor = "center", text = self._direct[self.strategy[0,0]], fill = "#C8C8C8", font = "Verdana " + str(int(-0.9 * self.case_px)) + " bold")                    
      
-    def dessin_grid(self, largeur, hauteur):
+    def _dessin_grid(self, largeur, hauteur):
+        """
+        Dessine les droites du grid de la grille.
+        """
         for i in range(self.grille.tab.shape[0] + 1):
             ni = self.case_px * i
             self._canevas.create_line(20, ni + 20, largeur - 20, ni + 20)
@@ -421,7 +487,11 @@ class Visualisation():
             nj = self.case_px * j
             self._canevas.create_line(nj + 20, 20, nj + 20, hauteur - 20)
     
-    def dessin_couleur(self):
+    def _dessin_couleur(self):
+        """
+        Dessine les cases de la grille dans le mode 'couleur'. On a un petit
+        circle coloré dans chaque case. 
+        """
         circle = max(self.case_px // 6, 2)
         
         for i in range(self.grille.tab.shape[0]):
@@ -437,7 +507,10 @@ class Visualisation():
                     self._canevas.create_rectangle(x0, y0, x0 + self.case_px, y0 + self.case_px, fill = "#5E5E64")
         
     def dessin_chiffre(self):
-
+        """
+        Dessine les cases de la grille dans le mode 'chiffre'. On a un chiffre
+        coloré dans chaque case. 
+        """
         for i in range(self.grille.tab.shape[0]):
             for j in range(self.grille.tab.shape[1]):
                 x0 = 20 + self.case_px * j 
@@ -452,6 +525,38 @@ class Visualisation():
                     self._canevas.create_rectangle(x0, y0, x0 + self.case_px, y0 + self.case_px, fill = "#5E5E64")       
 
 def pol_valeur(grille, gamma, M, eps = 1e-5, mode = "couleur"):
+    """
+    Calcule la stratégie optimale pour une grille donnée avec un gamma et une 
+    récompense finale passées en argument, en utilisant l'algorithme 
+    d'itération de la valeur. Le critère d'arrêt de l'approximation est aussi 
+    passé en argument, ainsi que le mode (i.e quel coûts utiliser pour le 
+    calcul).
+
+    Parameters
+    ----------
+    grille : Grille
+        La Grille pour laquelle on calcule la stratégie optimale.
+    gamma : float
+        Le gamma (taux d'amortissement) utilisé dans le calcul.
+    M : int
+        La récompense de la case but.
+    eps : float
+        Le critère d'arrêt utilisé dans le calcul. Le défaut est 1e-5.
+    mode : String
+        Avec quel coût calculer la stratégie. Deux modes: 'couleur' et 
+        'somme_chiffre'.
+        'couleur' calcule la stratégie avec les coûts en tab_cost (partie 2 de 
+        l'énoncé).
+        'somme_chiffre' calcule la stratégie avec la somme des coûts en chiffre 
+        (partie 4a de l'enoncé). Le défaut est 'couleur'.
+
+    Returns
+    -------
+    pol : numpy.ndarray
+        Tableau 2D représentant une stratégie pure. 
+    cpt : int
+        La quantité d’itérations avant la convergence de l'algorithme.
+    """
     vs = np.zeros(grille.tab.shape)
     vs[-1, -1] = M / (1 - gamma)
     erreur = 1 + eps
@@ -487,7 +592,38 @@ def pol_valeur(grille, gamma, M, eps = 1e-5, mode = "couleur"):
     return pol, cpt
 
 
-def pol_pl_mixte(grille, gamma, M, verbose = False, mode = "couleur"):
+def pol_pl_mixte(grille, gamma, M, mode = "couleur", verbose = False):
+    """    
+    Calcule la stratégie optimale pour une grille donnée, avec un gamma et une 
+    récompense finale passées en argument, en utilisant un PL. Le mode indique 
+    quels coûts utiliser pour le calcul. La stratégie calculé est 'mixte' (une 
+    distribution de probabilités). 
+
+    Parameters
+    ----------
+    grille : Grille
+        La Grille pour laquelle on calcule la stratégie optimale.
+    gamma : float
+        Le gamma (taux d'amortissement) utilisé dans le calcul.
+    M : int
+        La récompense de la case but.
+    mode : String
+        Avec quel coût calculer la stratégie. Deux modes: 'couleur' et 
+        'somme_chiffre'.
+        'couleur' calcule la stratégie avec les coûts en tab_cost (partie 2 de 
+        l'énoncé).
+        'somme_chiffre' calcule la stratégie avec la somme des coûts en chiffre 
+        (partie 4a de l'enoncé). Le défaut est 'couleur'.
+    verbose : bool
+        If True, shows the resolution of the PL calculated by Gurobi. Le défaut
+        est False.        
+
+    Returns
+    -------
+    pol : numpy.ndarray
+        Tableau 3D représentant une stratégie mixte. 
+    """
+
     # On créé le pl
     pl = gp.Model("mixte")
     if not verbose:
@@ -530,7 +666,37 @@ def pol_pl_mixte(grille, gamma, M, verbose = False, mode = "couleur"):
         strat = strat / strat.sum(2).reshape((lig, col, 1))  
     return strat
 
-def pol_pl_pure(grille, gamma, M, verbose = False, mode = "couleur"):
+def pol_pl_pure(grille, gamma, M, mode = "couleur", verbose = False):
+    """    
+    Calcule la stratégie optimale pour une grille donnée, avec un gamma et une 
+    récompense finale passées en argument, en utilisant un PLNE. Le mode 
+    indique quels coûts utiliser pour le calcul. La stratégie calculé est 
+    'pure' (une seule valeur par case). 
+
+    Parameters
+    ----------
+    grille : Grille
+        La Grille pour laquelle on calcule la stratégie optimale.
+    gamma : float
+        Le gamma (taux d'amortissement) utilisé dans le calcul.
+    M : int
+        La récompense de la case but.
+    mode : String
+        Avec quel coût calculer la stratégie. Deux modes: 'couleur' et 
+        'somme_chiffre'.
+        'couleur' calcule la stratégie avec les coûts en tab_cost (partie 2 de 
+        l'énoncé).
+        'somme_chiffre' calcule la stratégie avec la somme des coûts en chiffre 
+        (partie 4a de l'enoncé). Le défaut est 'couleur'.
+    verbose : bool
+        If True, shows the resolution of the PLNE calculated by Gurobi. Le 
+        défaut est False.        
+
+    Returns
+    -------
+    pol : numpy.ndarray
+        Tableau 2D représentant une stratégie pure. 
+    """
     # On créé le pl
     pl = gp.Model("mixte")
     if not verbose:
@@ -583,6 +749,31 @@ def pol_pl_pure(grille, gamma, M, verbose = False, mode = "couleur"):
 
 
 def pol_pl_mixte_mo(grille, gamma, M, verbose = False):
+    """    
+    Calcule la stratégie optimale pour une grille donnée, avec un gamma et une 
+    récompense finale passées en argument, en utilisant un PL. Cette fonction 
+    est spécifique pour la grille multi-objectifs avec comme fonction 
+    d'agrégation un critère max min(celle de la partie 4b). 
+    La stratégie calculé est 'mixte' (une distribution de probabilités). 
+
+    Parameters
+    ----------
+    grille : Grille
+        La Grille pour laquelle on calcule la stratégie optimale.
+    gamma : float
+        Le gamma (taux d'amortissement) utilisé dans le calcul.
+    M : int
+        La récompense de la case but.
+    verbose : bool
+        If True, shows the resolution of the PL calculated by Gurobi. Le défaut
+        est False.        
+
+    Returns
+    -------
+    pol : numpy.ndarray
+        Tableau 3D représentant une stratégie mixte. 
+    """
+    
     # On créé le pl
     pl = gp.Model("mixte")
     if not verbose:
@@ -637,6 +828,26 @@ def pol_pl_mixte_mo(grille, gamma, M, verbose = False):
 
 
 def tester_temps(fonction, list_grille, repeat = 10, **kwargs):
+    """
+    
+    Parameters
+    ----------
+    fonction : Function
+        La fonction qu'on veut tester le temps d'exécution. Il faut qu'elle 
+        prenne une grille en argument.
+    list_grille : list(Grille)
+        Liste avec toutes les Grilles qui doivent être testés.
+    repeat : int
+        La quantité de fois chaque Grille aura une stratégie calculé. Le défaut
+        est 10.
+    **kwargs : 
+        Keyword arguments necessaires à la fonction testée.
+
+    Returns
+    -------
+    float
+        Le temps moyen de calcul de la fonction passée en argument.
+    """
     temps = time.process_time()
     for grille in list_grille:
         for _ in range(repeat):
